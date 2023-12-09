03004561b72d3badb4813cb207e26b986e5d8b0d
yu51a5 <130870321+yu51a5@users.noreply.github.com>
2023-12-09 08:37:04 +0000

    Now querying for the ancient and the most recent stackoverflow questions

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
+import datetime
 
-url = f"https://api.stackexchange.com/2.3/questions?tagged=stackexchange-api&site=stackoverflow"
-result = requests.get(url)
-print(result.text) 
+for which, sort_order in (('very first', 'asc'), ('most recent', 'desc')):
+  url = f"https://api.stackexchange.com/2.3/questions?page=1&pagesize=1&order={sort_order}&sort=creation&site=stackoverflow"
+  result = requests.get(url).json().get("items")[0]
+  dt = datetime.datetime.fromtimestamp(result['creation_date']).strftime('%Y %b %d at %H:%M:%S')
+  print(f"The {which} Stack Overflow question was created on {dt}.\nIts title is `{result['title']}`.\n") 
